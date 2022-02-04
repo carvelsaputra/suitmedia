d5efcdbb7f3ed71fb04eb539ac885220d61bf5f4
Carvel Saputra Martaloho <carvel.saputra@gmail.com>
2022-02-04 07:18:33 +0700

    make dialog register

--- src/components/common/Dialog.vue
+++ src/components/common/Dialog.vue
@@ -10,47 +10,16 @@
         </div>
 
         <div class="modal-body">
-          <slot name="body">
-            <div class="form-wrapper">
-              <text-field>
-                <template v-slot:inputText>
-                  <label for="email" class="form-label">Email</label>
-                  <input
-                    class="text-field"
-                    id="email"
-                    placeholder="Email"
-                    type="text"
-                  />
-                </template>
-                <template v-slot:icon>
-                  <i class="fas fa-user"></i>
-                </template>
-              </text-field>
-            </div>
-            <div class="form-wrapper">
-              <text-field>
-                <template v-slot:inputText>
-                  <label for="password" class="form-label">Password</label>
-
-                  <input
-                    class="text-field"
-                    placeholder="Password"
-                    type="password"
-                  />
-                </template>
-                <template v-slot:icon>
-                  <i class="fas fa-lock"></i>
-                </template>
-              </text-field>
-            </div>
-          </slot>
+          <slot name="body"> </slot>
         </div>
 
         <div class="modal-footer">
           <slot name="footer">
-            <button class="modal-default-button" @click="$emit('close')">
-              OK
-            </button>
+            <button
+              class="modal-default-button"
+              @click="$emit('close')"
+              v-text="title"
+            />
           </slot>
         </div>
       </div>
@@ -58,24 +27,11 @@
   </div>
 </template>
 <script>
-import TextField from "@/components/form/TextField";
 export default {
   props: {
     title: {
       type: String,
     },
   },
-  components: {
-    TextField,
-  },
 };
 </script>
-<style>
-.form-label {
-  position: absolute;
-  margin-top: -20px;
-}
-.form-wrapper {
-  margin-top: 30px;
-}
-</style>
--- src/components/navigation/Group.vue
+++ src/components/navigation/Group.vue
@@ -1,15 +1,103 @@
 <template>
   <List v-for="(item, i) in list" :key="i" v-bind="item" @click="showDialog" />
+  <!-- Dialog Login -->
   <Dialog
     v-if="showDialogLogin"
     :title="dialogTitle"
     @close="showDialogLogin = false"
   >
+    <template v-slot:body>
+      <div class="form-wrapper">
+        <text-field>
+          <template v-slot:inputText>
+            <label for="email" class="form-label">Email</label>
+            <input
+              class="text-field"
+              id="email"
+              placeholder="Email"
+              type="text"
+            />
+          </template>
+          <template v-slot:icon>
+            <i class="fas fa-envelope"></i>
+          </template>
+        </text-field>
+      </div>
+      <div class="form-wrapper">
+        <text-field>
+          <template v-slot:inputText>
+            <label for="password" class="form-label">Password</label>
+
+            <input class="text-field" placeholder="Password" type="password" />
+          </template>
+          <template v-slot:icon>
+            <i class="fas fa-lock"></i>
+          </template>
+        </text-field>
+      </div>
+    </template>
   </Dialog>
+  <!-- Dialog Login -->
+
+  <!-- Dialog Register -->
+  <Dialog
+    v-if="showDialogRegister"
+    :title="dialogTitle"
+    @close="showDialogRegister = false"
+  >
+    <template v-slot:body>
+      <div class="form-wrapper">
+        <text-field>
+          <template v-slot:inputText>
+            <label for="Name" class="form-label">Name</label>
+            <input
+              class="text-field"
+              id="Name"
+              placeholder="Name"
+              type="text"
+            />
+          </template>
+          <template v-slot:icon>
+            <i class="fas fa-user"></i>
+          </template>
+        </text-field>
+      </div>
+      <div class="form-wrapper">
+        <text-field>
+          <template v-slot:inputText>
+            <label for="email" class="form-label">Email</label>
+            <input
+              class="text-field"
+              id="email"
+              placeholder="Email"
+              type="text"
+            />
+          </template>
+          <template v-slot:icon>
+            <i class="fas fa-envelope"></i>
+          </template>
+        </text-field>
+      </div>
+      <div class="form-wrapper">
+        <text-field>
+          <template v-slot:inputText>
+            <label for="password" class="form-label">Password</label>
+
+            <input class="text-field" placeholder="Password" type="password" />
+          </template>
+          <template v-slot:icon>
+            <i class="fas fa-lock"></i>
+          </template>
+        </text-field>
+      </div>
+    </template>
+  </Dialog>
+  <!-- Dialog Register -->
 </template>
 <script>
 import List from "@/components/navigation/List.vue";
 import Dialog from "@/components/common/Dialog";
+import TextField from "@/components/form/TextField";
 
 export default {
   props: {
@@ -20,6 +108,7 @@ export default {
   components: {
     List,
     Dialog,
+    TextField,
   },
   data() {
     return {
@@ -35,7 +124,23 @@ export default {
         this.showDialogLogin = !this.showDialogLogin;
         this.dialogTitle = params;
       }
+      if (params == "Register") {
+        this.showDialogRegister = !this.showDialogRegister;
+        this.dialogTitle = params;
+      }
     },
   },
 };
 </script>
+<style scoped>
+.text-field {
+  background-color: #eeeeee;
+}
+.form-label {
+  position: absolute;
+  margin-top: -20px;
+}
+.form-wrapper {
+  margin-top: 30px;
+}
+</style>
