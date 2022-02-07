7268b814457d6262f64121eaa66d4493655fb62b
Carvel Saputra Martaloho <carvel.saputra@gmail.com>
2022-02-07 21:39:46 +0700

    make responsive

--- src/components/thread/ThreadTop.vue
+++ src/components/thread/ThreadTop.vue
@@ -1,15 +1,17 @@
 <template>
-  <aside class="threads-top-wrapper">
-    <h3 class="top-title">Diskusi 5 Teratas</h3>
-    <div class="threads-content">
-      <ul>
-        <li class="threads-item" v-for="(item, i) in list" :key="i">
-          <div class="threads-number">{{ i + 1 }}</div>
-          <div class="threads-title">
-            <a href="#">{{ item }}</a>
-          </div>
-        </li>
-      </ul>
+  <aside>
+    <div class="threads-top-wrapper">
+      <h3 class="top-title">Diskusi 5 Teratas</h3>
+      <div class="threads-content">
+        <ul>
+          <li class="threads-item" v-for="(item, i) in list" :key="i">
+            <div class="threads-number">{{ i + 1 }}</div>
+            <div class="threads-title">
+              <a href="#">{{ item }}</a>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </aside>
 </template>
