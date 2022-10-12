d8beb22f0ff27040bc64e8ded9f3887666d55c2d
Carvel Saputra Martaloho <carvel.saputra@gmail.com>
2022-10-12 21:09:29 +0700

    create spacing for search bar

--- src/components/form/TextField.vue
+++ src/components/form/TextField.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-field-wrapper" :style="customStyle">
     <slot name="inputText">
-      <input class="text-field" placeholder="Search" type="text" />
+      <input class="text-field" style="padding-left:20px;box-sizing:border-box" placeholder="Search" type="text" />
     </slot>
     <slot name="icon">
       <i class="fas fa-search"></i>
