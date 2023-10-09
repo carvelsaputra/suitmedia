18f420368bef33ee30899c221fd9a28e5a18cce2
carvel saputra <carvel.saputra@gmail.com>
2023-10-09 23:36:56 +0700

    create documentaion at state management

--- src/store/comments.js
+++ src/store/comments.js
@@ -6,17 +6,27 @@ export const comment = {
     list: [],
   }),
   actions: {
+    /**
+     *
+     * @returns a list contain comments of content from local json
+     */
     fetchList({ commit }) {
       return axios.get("http://localhost:8080/comments.json").then((res) => {
         commit("setList", res.data);
       });
     },
   },
+  /**
+   * @return a list of comment, we can map or process list of comment data below
+   */
   mutations: {
     setList(state, newList) {
       state.list = newList;
     },
   },
+  /**
+   * @return a list if we want to get from frontend
+   */
   getters: {
     list: (state) => {
       return state.list.map((row) => {
